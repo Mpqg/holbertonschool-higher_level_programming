843a7c8af32ce0ccb9c876dae4bc187513ce8f86
Mpqg <4534@holbertonschool.com>
2022-05-09 23:00:52 -0500

    fixing code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        for y in x:
-            print("{}".format(y), end=" ")
-            print("")
+        for y in range(len(x)):
+            print("{:d}".format(x[y]), end="")
+            if (y == len(x) - 1):
+                print(" ", end="")
+        print("")
