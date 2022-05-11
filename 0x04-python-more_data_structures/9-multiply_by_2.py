65031dd24e9e4fe83e8aa606c51c8a7e89d0c939
Mpqg <4534@holbertonschool.com>
2022-05-10 21:54:23 -0500

    fixing code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_dictionary = {}
+    new_dict = {}
     for i, j in a_dictionary.items():
-        new_dictionary[i] = j * 2
-        return new_dictionary
+        new_dict[i] = j * 2
+    return new_dict
