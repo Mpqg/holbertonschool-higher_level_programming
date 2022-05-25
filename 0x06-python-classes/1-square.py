425e2e12291b2e625b414160965f5c9660a33a1a
Mpqg <4534@holbertonschool.com>
2022-05-24 21:18:18 -0500

    attribute size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
-2-square.py
+#!/usr/bin/python3
+# 1-square.py
+"""Define a square."""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size):
+        """Initialize a new square."""
+        self.size = size
