ab6f19621c595b8c915a877f6bbaaea09806ed7f
Mpqg <4534@holbertonschool.com>
2022-06-14 22:19:02 -0500

    tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,16 @@
+"""
+Unit test for the Rectangle class
+"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    def test_instantiation(self):
+        """
+        Rectangle initializate
+        """
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
