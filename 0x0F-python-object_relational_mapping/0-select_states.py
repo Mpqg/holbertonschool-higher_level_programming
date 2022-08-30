4d4aee750af1beeadf072350a65e8694b372d66d
Mpqg <4534@holbertonschool.com>
2022-08-30 15:53:26 -0500

    Fixing codeee

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa:
-"""
+# Lists all states from the database hbtn_0e_0_usa.
 
-if __name__ == '__main__':
-    """ Module to select states"""
-    import MySQLdb
-    import sys
+import sys
+import MySQLdb
 
-    db = MySQLdb.connect(
-        host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-
-    sql = "SELECT * FROM states ORDER BY states.id ASC"
-
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-
-    for i in rows:
-        print(i)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
